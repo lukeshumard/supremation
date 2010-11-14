2658d2cfeca4a9b4e0671e9b75e2187b4a1cde14
Luke Shumard <lukeshumard@gmail.com>
2010-11-14 12:33:27 +0000

    new logic

--- jquery.supermate.js
+++ jquery.supermate.js
@@ -14,46 +14,58 @@
 	
 	$.fn.supermate = function(properties,speed,easing,callback){
 		
-		// CALLING ALL FUNCTIONS
-			    
-	    function areSelectorsValid(opts){
-	    	for (var key in opts){
-		        //if (/* NO MATCH */){
-		        //    debug('Your ' + key + ' found no elements.');    
-		        //    return false;
-		        //} // ELSE, MATCH
-		        return true;
-		    }
-	    }
-	    
+		// CALLING ALL FUNCTIONS	    
 	    function determineRatio() {
-	    	// 1. Get beginning state
-	    	// 2. Get end state
-	    	// 3. Determine total distance
-	    	// 5. Distance * (inverse of ratio) = duration
-	    	beginning = 10;
-	    	end = 800;
-	    	distance = end - beginning;
-	    	duration = distance * (1 / speed);
+	    	selectorClone = $(selector).clone();
+	    	// THIS WORKS, SO WHY NOT BELOW
+	    	// selector.css({left: '200px'});
+	    	
+	    	selectorClone.css({left: '200px'});
+	    	// console.log(properties);
+	    	// console.log(selector.css('left'));
+	    	// console.log(selectorClone.css('left'));
+	    	// append selectorClone to use new css properties
+	    	
+	    	function distance(selector, selectorClone, attr) {
+	    		val1 = selector.css(attr).match(/\d+/g);
+	    		val2 = selectorClone.css(attr).match(/\d+/g);
+	    		// console.log(val1 + ' / ' + val2);
+	    		// console.log(Math.abs(val1 - val2));
+	    		return Math.abs(val1 - val2);
+	    	}
+	    	
+	    	dist = /*Math.max(*/[
+		    	distance(selector, selectorClone, "top"),
+		    	distance(selector, selectorClone, "left"),
+		    	distance(selector, selectorClone, "width"),
+		    	distance(selector, selectorClone, "height")
+	    	]/*)*/;
+	    	// console.log(dist);
+	    	
+	    	// beginning = 10;
+	    	// end = 800;
+	    	// distanceProof = end - beginning;
+	    	duration = dist * (1 / speed);
+	    	selectorClone.remove();
 	    }
 	    
-	    function initAnimation(selector) {
-	    	console.log(duration);
+	    function initAnimation() {
+	    	// console.log(duration);
 	    	selector.animate(properties,duration,easing,callback.call());
 	    }
 	    
 	    // BEGIN
 	    var opts	= $.supermate,
-        beginning, end, distance, duration;
+	    selector	= $(this),
+        max, dist, distance, val1, val2,
+        beginning, end, distanceProof, duration;
         
         // DEFINE CALLBACK ARGUEMENT AS A FUNCTION
         callback    = callback || function(){};
         
-        // CHECK FOR VALID OPTIONS
-        // if (!areSelectorsValid()){ return false; }
-        
+                
         determineRatio();
-        initAnimation($(this));
+        initAnimation();
         
         return this;
         
