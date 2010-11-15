ab5691590aec8ad2d272bcb71e85000b0694eca7
Luke Shumard <lukeshumard@gmail.com>
2010-11-15 01:55:54 +0000

    dave made huge changes. i changed a number

--- jquery.supermate.js
+++ jquery.supermate.js
@@ -1,81 +1,85 @@
 /*
 + Supermate
 + copyright Luke Shumard & Dave Donahue, licensed GPL & MIT
-+ version 0.01
++ version 0.1
 
 + Documentation: xxx
-+ GitHub: https://github.com/lukeshumard/Supermate
++ GitHub: https://github.com/lukeshumard/supermate
 
 + lukeshumard.com
 + monowi.net
 
 */
-;(function($){
-	
-	$.fn.supermate = function(properties,speed,easing,callback){
-		
-		// CALLING ALL FUNCTIONS	    
-	    function determineRatio() {
-	    	selectorClone = $(selector).clone();
-	    	// THIS WORKS, SO WHY NOT BELOW
-	    	// selector.css({left: '200px'});
-	    	
-	    	selectorClone.css({left: '200px'});
-	    	// console.log(properties);
-	    	// console.log(selector.css('left'));
-	    	// console.log(selectorClone.css('left'));
-	    	// append selectorClone to use new css properties
-	    	
-	    	function distance(selector, selectorClone, attr) {
-	    		val1 = selector.css(attr).match(/\d+/g);
-	    		val2 = selectorClone.css(attr).match(/\d+/g);
-	    		// console.log(val1 + ' / ' + val2);
-	    		// console.log(Math.abs(val1 - val2));
-	    		return Math.abs(val1 - val2);
-	    	}
-	    	
-	    	dist = /*Math.max(*/[
-		    	distance(selector, selectorClone, "top"),
-		    	distance(selector, selectorClone, "left"),
-		    	distance(selector, selectorClone, "width"),
-		    	distance(selector, selectorClone, "height")
-	    	]/*)*/;
-	    	// console.log(dist);
-	    	
-	    	// beginning = 10;
-	    	// end = 800;
-	    	// distanceProof = end - beginning;
-	    	duration = dist * (1 / speed);
-	    	selectorClone.remove();
-	    }
-	    
-	    function initAnimation() {
-	    	// console.log(duration);
-	    	selector.animate(properties,duration,easing,callback.call());
-	    }
-	    
-	    // BEGIN
-	    var opts	= $.supermate,
-	    selector	= $(this),
-        max, dist, distance, val1, val2,
-        beginning, end, distanceProof, duration;
-        
-        // DEFINE CALLBACK ARGUEMENT AS A FUNCTION
-        callback    = callback || function(){};
-        
-                
-        determineRatio();
-        initAnimation();
-        
-        return this;
-        
-        }; // end $.fn.supermate
-		
-		$.supermate = {     
-		        properties		: null,
-		        ratio			: null,
-		        easing			: null,
-	  	};
-	  	
-	
+; (function($) {
+
+  $.fn.supermate = function(properties, speed, easing, callback) {
+
+    // Get current element
+    var element = this;
+      
+    // Calculate the duration
+    duration = calcDuration(element, properties, speed);
+      
+    // Call .animate
+    $(element).animate(properties, duration, easing, callback);
+
+    // Calculate duration of animation
+    function calcDuration(element, properties, speed) {
+      
+      // Take measurements on original element (offset, height, width)
+      before = measurePosition(element);
+      
+      // Clone original element
+      cloned = element.clone().insertBefore(element);
+      
+      // Hide original element
+      element.hide();
+      
+      // Apply CSS to cloned element
+      $.each(properties, function(key, value) {
+        cloned.css(key, value);
+      });
+      
+      // Take new measurements
+      after = measurePosition(cloned);
+      
+      // Go back to normal
+      cloned.remove();
+      element.show();
+
+      // Compute distance between before and after
+      distance = calcDistance(before, after);
+      
+      // Compute duration
+      return distance / ( speed / 1000 );
+      
+    }
+    
+    // Calculate distance between two objects (by property)
+    function calcDistance(elem1, elem2) {
+      var max = 0; var distance = 0;
+      
+      $.each(elem1, function(key, value) {
+        distance = Math.abs( elem1[key] - elem2[key] );
+        max = (distance > max) ? distance : max;
+      });
+      
+      return max;
+    }
+    
+    function measurePosition(elem) {
+      top = $(elem).offset().top;
+      left = $(elem).offset().left;
+      width = $(elem).width();
+      height = $(elem).height();
+      
+      return {
+        "top" : top,
+        "left" : left,
+        "width" : width,
+        "height" : height
+      };
+    }
+    
+  }
 })(jQuery);
