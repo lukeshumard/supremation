7a7239e19f19d93fcc505601daa5408581b65ba0
Luke Shumard <lukeshumard@gmail.com>
2013-10-24 13:23:48 +0100

    refactored plugin

--- jquery.supremation.js
+++ jquery.supremation.js
@@ -1,80 +1,88 @@
-/*
-+ SUPREMATION
-+ copyright Luke Shumard & Dave Donahue, licensed GPL & MIT
-+ version 0.1
-
-+ Documentation: http://www.supremation.com
-+ GitHub: https://github.com/lukeshumard/supremation
-
-+ lukeshumard.com
-+ monowi.net
-
-*/
-; (function($) {
+/**
+ * SUPREMATION
+ * GitHub: https://github.com/lukeshumard/supremation
+ * copyright Luke Shumard & Dave Donahue, licensed GPL & MIT
+ * version 1.0
+ */
+;(function($) {
 
   // Extend jQuery with supermate function
   $.fn.supremate = function(properties, speed, easing, callback) {
 
-    // Get current element
-    var element = this;
-      
-    // Calculate the duration for .animate
-    duration = calcDuration(element, properties, speed);
-      
-    // Call .animate
-    $(element).animate(properties, duration, easing, callback);
-
-    // Given an element, an array of CSS changes, and a speed (in px/s), calculate the duration for .animate
-    function calcDuration(element, properties, speed) {
-      
-      // Get positioning info for original element
-      before = measurePosition(element);
-      
-      // Clone original element
-      cloned = element.clone().insertBefore(element);
-      
-      // Hide original element (so that cloned element can be placed in the exact position of the original)
-      element.hide();
-      
-      // Apply properties to cloned element
-      $(cloned).animate(properties,0)
-      
-      // Get positioning info for cloned element
-      after = measurePosition(cloned);
-      
-      // Revert to normal
-      cloned.remove();
-      element.show();
-
-      // Calculate the distance between before and after elements
-      distance = calcDistance(before,after);
-      
-      // Compute and return the duration (time = distance/speed)
-      
-      // console.log('distance: ' + distance);
-      // console.log('duration: ' + distance / ( speed / 1000 ) );
-      
-      return distance / ( speed / 1000 );     // Speed provided in px/s; convert to px/ms
-    }
+    var $el = $(this);                                                // Get current element
+    var duration = $.fn.supremate._duration($el, properties, speed);  // Calculate the duration for .animate
     
-    // Calculate distance between two objects
-    function calcDistance(elem1,elem2) {
-      var max = distance = 0;   
-      $.each(elem1, function(key, value) {
-        distance = Math.abs( elem1[key] - elem2[key] );
-        max = (distance > max) ? distance : max;
-      }); 
-      return max;
-    }
+    $el.animate(properties, duration, easing, callback);  // Call .animate
+
+    return this;  // Ensure chainability
+
+  }
+
+  /**
+   * Duration
+   * Calculate duration needed to maintain consistent speed
+   * @private
+   *
+   * @param $el {jQuery Object} Element to be animated
+   * @param properties {Object} CSS changes to element
+   * @param speed {Number} Speed of changes in px/s
+   *
+   * @returns {Integer} Duration for change at speed
+   */
+  $.fn.supremate._duration = function calcDuration($el, properties, speed) {
+
+    var before = this._position($el);               // Get positioning info for original element
+    var $cloned = $el.clone().insertBefore($el);    // Clone original element
+
+    $el.hide();                                     // Hide original element
+    $cloned.animate(properties,0);                  // Apply properties to cloned element
     
-    // Take a snapshot of positioning attributes of an object (offset, dimensions)
-    function measurePosition(elem) {
-      return {
-        "top" : $(elem).offset().top,
-        "left" : $(elem).offset().left,
-        "width" : $(elem).width(),
-        "height" : $(elem).height()
-      };
+    var after = this._position($cloned);            // Get positioning info for cloned element
+    
+    $cloned.remove();                               // Revert to normal
+    $el.show();                                     // Show original element
+
+    var distance = this._distance(before,after);    // Calculate the distance between before and after elements
+    
+    return distance / ( speed / 1000 );     // Speed provided in px/s; convert to px/ms
+  }
+
+  /**
+   * Distance
+   * Calculate distance between original element and cloned element
+   * @private
+   *
+   * @param original {Object} Original element position
+   * @param cloned {Object} Cloned element position
+   *
+   * @returns {Integer} Distance between objects
+   */
+  $.fn.supremate._distance = function calcDistance(before,after) {
+    var max = distance = 0;   
+    $.each(before, function(key, value) {
+      distance = Math.abs( before[key] - after[key] );
+      max = (distance > max) ? distance : max;
+    }); 
+    return max;
+  }
+
+  /**
+   * Position
+   * Take a snapshot of positioning attributes of an object (offset, dimensions)
+   * @private
+   *
+   * @param $el {jQuery Object} Element to capture position
+   *
+   * @returns {Object} Top, left, width, and height measurements of object
+   */
+  $.fn.supremate._position = function measurePosition($el) {
+    var offset = $el.offset();
+    return {
+      "top" : offset.top,
+      "left" : offset.left,
+      "width" : $el.width(),
+      "height" : $el.height()
     };
   }
+
 })(jQuery);
